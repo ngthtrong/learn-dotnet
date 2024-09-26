92050b576bb4f702337bb1d9fa7c900ef0e4cf55
ngthtrong <ngthtrong204@gmail.com>
2024-09-26 15:32:59 +0700

    done exer3

--- obj/Debug/net8.0/learn-dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/learn-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learn-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bc06be3682f2a3c9706720aa7bec69d9997765b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8621279932d70429de55ec4b68f55c29a2f77ac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("learn-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learn-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
